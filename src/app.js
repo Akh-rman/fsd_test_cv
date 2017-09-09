91e7d015a7e00afb4eb3be86c27513ee1b382dfb
Akh-rman <akh-rman@rambler.ru>
2017-09-09 13:50:13 +0300

    Написал первый блок. Написал первый блок app. Переименовал директорию images в icons. Обновил иконку страницы.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 import "./css/normilize.css";
+import App from "./blocks/app/app.jsx";
+//import "./blocks/logo/logo.jsx";
+
+ReactDOM.render(
+    <App class = "app"/>,
+    document.getElementById("main")
+)
---
+++ src/blocks/app/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./app.scss";
+
+export default function App (props) {
+    return (
+        <div className = {props.class}></div>
+    );
+}

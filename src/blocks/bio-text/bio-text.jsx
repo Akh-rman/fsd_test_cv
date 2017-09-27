607e0baa2a38bf3bcc58520e7781c465b9417e51
Akh-rman <akh-rman@rambler.ru>
2017-09-27 18:13:46 +0300

    Изменил блоки bio-text и bio-profile. Внес изменения в блоки bio-text и bio-profile. В блоке bio-text изменил функциональный компонент на классовый. В блоке bio-profile добавил тег в текст.

--- src/blocks/bio-text/bio-text.jsx
+++ src/blocks/bio-text/bio-text.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 import "./bio-text.scss";
+import "./bio-text_bold.scss";
 
-export default function BioText (props) {
-    return (
-        <p className = {props.class}>{props.text}</p>
-    );
+export default class BioText extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.text = this.props.text;
+    }
+
+    createText() {
+        return {__html: this.props.text}
+    }
+
+    render() {
+        return (
+            <p className = {this.props.class} dangerouslySetInnerHTML = {this.createText()}></p>
+        );
+    }
 }
